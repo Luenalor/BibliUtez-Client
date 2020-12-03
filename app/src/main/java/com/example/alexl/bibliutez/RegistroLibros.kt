088902417b9457863538be47ba778174e3be6f53
AlexisLoya <alexloy117@gmail.com>
2020-12-02 22:13:33 -0600

    add books implement

--- app/src/main/java/com/example/alexl/bibliutez/RegistroLibros.kt
+++ app/src/main/java/com/example/alexl/bibliutez/RegistroLibros.kt
@@ -3,8 +3,6 @@ package com.example.alexl.bibliutez
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.RecyclerView
-import android.text.Editable
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
@@ -13,25 +11,21 @@ import com.example.alexl.bibliutez.model.categorias.CategoriasBean
 import com.example.alexl.bibliutez.model.categorias.CategoriasJsonPlaceHolder
 import com.example.alexl.bibliutez.model.libros.LibrosJsonPlaceHolder
 import com.example.alexl.bibliutez.model.libros.LibrosBean
-import kotlinx.android.synthetic.main.gerente_lista_libros.*
 import kotlinx.android.synthetic.main.registro_libros.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import android.widget.EditText
-
-
 
 
 class RegistroLibros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registro_libros)
-        var m_categorias: List<CategoriasBean>
+
 
         btnRegistroConsultarLibros.setOnClickListener {
             var consulta = Intent(this, ListaLibrosActivity::class.java)
@@ -40,35 +34,38 @@ class RegistroLibros : AppCompatActivity() {
 
         //---Mostrar Categorias----
 
-        //Retrofit builder
-        val retrofit = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl("http://192.168.0.6:8080/BibliUtez_war/categorias/")
-            .build()
 
-        //object to call methods
-        val jsonPlaceHolderApi = retrofit.create(CategoriasJsonPlaceHolder::class.java)
-        val call: Call<List<CategoriasBean>> = jsonPlaceHolderApi.categoriasFindAll()
+        //Spinner
+        var sp_categoria_posicion: Int = 1
+        var categoria: CategoriasBean = CategoriasBean(0, "")
+        var sp_categoria = findViewById<Spinner>(R.id.spnLibroCategoria)
+        sp_categoria.onItemSelectedListener =
+                object : AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                        sp_categoria_posicion = position + 1
 
+                    }
 
-        call.enqueue(object : Callback<List<CategoriasBean>> {
-            override fun onFailure(call: Call<List<CategoriasBean>>, t: Throwable) {
-                Log.e("onFailure!!!!!Categoria", t.message.toString())
-            }
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
 
-            override fun onResponse(call: Call<List<CategoriasBean>>, response: Response<List<CategoriasBean>>) {
-                try {
-                    m_categorias =  response.body()!!
-                    llenar(m_categorias)
-                }catch (e:Exception){
+                    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                        TODO("Not yet implemented")
+                    }
 
                 }
-            }
 
-        })
 
-        btnRegistroLibro.setOnClickListener{
-            try{
+        fun llenar(categoriasBean: CategoriasBean) {
+            categoria = categoriasBean
+
+        }
+
+
+
+        btnRegistroLibro.setOnClickListener {
+            try {
                 val titulo = findViewById<EditText>(R.id.txtRegistroLibroTitulo).text.toString()
                 val editorial = findViewById<EditText>(R.id.txtRegistroLibroEditorial).text.toString()
                 val autor = findViewById<EditText>(R.id.txtRegistroAutor).text.toString()
@@ -78,26 +75,37 @@ class RegistroLibros : AppCompatActivity() {
                 val num_pag = Integer.parseInt((input2.text.toString()))
                 val input3 = findViewById(R.id.txtRegistroLibroStock) as EditText
                 val stock = Integer.parseInt((input3.text.toString()))
-                //Spinner
-                val sp_categoria = findViewById<Spinner>(R.id.spnLibroCategoria)
-                sp_categoria.onItemSelectedListener = object : AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
-                    override fun onNothingSelected(parent: AdapterView<*>?) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+                //Retrofit builder
+                val retrofit2 = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("http://192.168.0.6:8080/BibliUtez_war/categorias/")
+                    .build()
+
+                //object to call methods
+                val jsonPlaceHolderApi2 = retrofit2.create(CategoriasJsonPlaceHolder::class.java)
+                val call: Call<CategoriasBean> = jsonPlaceHolderApi2.categoriasFindOne(sp_categoria_posicion)
+                call.enqueue(object : Callback<CategoriasBean> {
+                    override fun onFailure(call: Call<CategoriasBean>, t: Throwable) {
+
                     }
 
-                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    override fun onResponse(call: Call<CategoriasBean>, response: Response<CategoriasBean>) {
+                        llenar(response.body()!!)
                     }
 
-                    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                    }
-
-                }
-
+                })
                 //Llenar object libroBean
-                val libro:LibrosBean = LibrosBean(
-                    0,titulo.toString(),editorial.toString(),autor.toString(),CategoriasBean(0,"Novela de Aventura"),precio.toDouble(),num_pag.toInt(),stock.toInt())
+                val libro: LibrosBean = LibrosBean(
+                    0,
+                    titulo.toString(),
+                    editorial.toString(),
+                    autor.toString(),
+                    categoria,
+                    precio.toDouble(),
+                    num_pag.toInt(),
+                    stock.toInt()
+                )
 
 
                 //Retrofit builder
@@ -120,11 +128,11 @@ class RegistroLibros : AppCompatActivity() {
                     override fun onResponse(call: Call<Int>, response: Response<Int>) {
                         Toast.makeText(
                             this@RegistroLibros,
-                            "Agregado correctamente${libro.nombre}", Toast.LENGTH_LONG
+                            "Agregado correctamente", Toast.LENGTH_LONG
                         ).show()
                     }
                 })
-            }catch (e:Exception){
+            } catch (e: Exception) {
                 Toast.makeText(
                     this@RegistroLibros,
                     "Ocurrió un error ${e}", Toast.LENGTH_LONG
@@ -135,13 +143,8 @@ class RegistroLibros : AppCompatActivity() {
 
 
     }
-    fun llenar(listaCategorias: List<CategoriasBean>){
-        var adapter:ArrayAdapter<String>
-            adapter = ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item)
 
-        }
-
-    }
+}
 
 
 
--- app/src/main/java/com/example/alexl/bibliutez/model/categorias/CategoriasJsonPlaceHolder.kt
+++ app/src/main/java/com/example/alexl/bibliutez/model/categorias/CategoriasJsonPlaceHolder.kt
@@ -2,14 +2,14 @@ package com.example.alexl.bibliutez.model.categorias
 
 import com.example.alexl.bibliutez.model.libros.LibrosBean
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.PUT
+import retrofit2.http.*
 
 interface CategoriasJsonPlaceHolder {
     @GET("findAll")
     fun categoriasFindAll(): Call<List<CategoriasBean>>
+
+    @GET("findOne")
+    fun categoriasFindOne(@Query("id") id: Int): Call<CategoriasBean>
 
     @POST("add")
     fun categoriasAdd(@Body libro: CategoriasBean): Call<CategoriasBean>
